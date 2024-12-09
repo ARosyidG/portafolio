ed3c1e228a0a5fc572b59240ac10e9f1f96d9309
arosyidg <rosyidkeren164@gmail.com>
2024-12-09 18:50:15 +0800

    fix image path

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,7 +6,7 @@ export default function AboutContainer() {
             <div className="px-10 py-2 text-center md:text-left md:max-h-96 text-clip overflow-hidden md:border-l border-t md:border-t-0">
                 <div className="my-4 flex flex-wrap md:flex-nowrap border md:border-0 p-10 md:p-0 border-slate-700 justify-center md:justify-normal items-center">
                     <div className="rounded-full overflow-hidden h-20 w-20">
-                        <Image src="/Me.jpg" alt="Me" width={100} height={100}></Image>
+                        <Image src="./Me.jpg" alt="Me" width={100} height={100}></Image>
                     </div>
                     <div className="md:ml-4">
                         <div className="text-2xl">AHMAD ROSYID GANAUSI</div>
@@ -23,12 +23,12 @@ export default function AboutContainer() {
                     <div className="flex justify-center items-center md:justify-start md:items-start">
                         <div className="w-fit h-fit mx-2 rounded-xl overflow-hidden">
                             <a href="https://github.com/ARosyidG" target="_blank">
-                                <Image src="/github-Logo.jpg" width={30} height={30} alt="github account"></Image>
+                                <Image src="./github-Logo.jpg" width={30} height={30} alt="github account"></Image>
                             </a>
                         </div>
                         <div className="w-fit h-fit mx-2 rounded-xl overflow-hidden">
                             <a href="https://www.instagram.com/ganausi/" target="_blank">
-                                <Image src="/instagram-Logo.jpg" width={30} height={30} alt="github account"></Image>
+                                <Image src="./instagram-Logo.jpg" width={30} height={30} alt="github account"></Image>
                             </a>
                         </div>
                     </div>
@@ -40,22 +40,22 @@ export default function AboutContainer() {
                 </div>
                 <div className="grid grid-cols-3 gap-1 grid-rows-3 aspect-[1/1]">
                     <div className="flex justify-center items-center col-span-2 bg-slate-50 p-1">
-                        <Image src="/UnityLogo.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./UnityLogo.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                     <div className="flex justify-center items-center bg-slate-400 p-1">
-                        <Image src="/NextJs.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./NextJs.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                     <div className="flex justify-center items-center bg-slate-50 p-1">
-                        <Image src="/BlenderLogo.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./BlenderLogo.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                     <div className="flex justify-center items-center col-span-2 bg-slate-800 p-1">
-                        <Image src="/springbootlogo.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./springbootlogo.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                     <div className="flex justify-center items-center col-span-2 bg-slate-400 p-1">
-                        <Image src="/LaravelLogo.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./LaravelLogo.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                     <div className="flex justify-center items-center bg-slate-200 p-1">
-                        <Image src="/GodotLogo.png" width={200} height={200} alt="Unity"></Image>
+                        <Image src="./GodotLogo.png" width={200} height={200} alt="Unity"></Image>
                     </div>
                 </div>
             </div>
