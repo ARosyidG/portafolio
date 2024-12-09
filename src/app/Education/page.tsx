75b0fff935f92265718fca9c62eaf39794f22e5a
arosyidg <rosyidkeren164@gmail.com>
2024-12-09 18:17:34 +0800

    edit education

--- src/app/Education/page.tsx
+++ src/app/Education/page.tsx
@@ -1,61 +1,60 @@
 export default function EducationContainer() {
     return (
         <div className="px-10 py-2 md:max-h-96 overflow-hidden border-x">
-            <div className="table-auto">
-                <tr>
-                    <td className="pb-2 align-bottom">
-                        <div className="-rotate-90 h-2 w-2">
-                            2019
-                        </div>
-                    </td>
-                    <td>
-                        <div className="py-2 pl-5 pr-10">
-                            <h1 className="font-bold text-2xl">sadasd</h1>
-                            <p className="text-gray-500">sadsadasdsadsa</p>
-                            <p className="text-gray-500">asdsadasdsadsadasadsa</p>
-                            <p className="text-gray-500">asdsaasdsadsadsadsadsadasdsa</p>
-                        </div>
-                    </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td className="">
-                        <div className="w-2 h-2 bg-white border rounded-full"></div>
-                    </td>
-                    <td className="flex justify-center">
-                        <div className="w-full h-2 bg-white border rounded-full"></div>
-                    </td>
-                    <td className="">
-                        <div className="w-2 h-2 bg-white border rounded-full"></div>
-                    </td>
-                    <td className="flex justify-center">
-                        <div className="w-full h-2 bg-white border rounded-full"></div>
-                    </td>
-                    <td className="">
-                        <div className="w-2 h-2 bg-white border rounded-full"></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td className="pt-2 align-top">
-                        <div className="rotate-90 h-2 w-2">
-                            2020
-                        </div>
-                    </td>
-                    <td>
-                        <div className="py-2 pl-5 pr-10">
-                            <h1 className="font-bold text-2xl">sadasd</h1>
-                            <p className="text-gray-500">sadsadasdsadsa</p>
-                            <p className="text-gray-500">asdsadasdsadsadasadsa</p>
-                            <p className="text-gray-500">asdsaasdsadsadsadsadsadasdsa</p>
-                        </div>
-                    </td>
-                    <td></td>
-                </tr>
-            </div>
+            <table className="">
+                <tbody className="table-auto">
+                    <tr>
+                        <td className="pb-2 align-bottom">
+                            <div className="-rotate-90 h-2 w-2 -translate-x-[300%]">
+                                2019-2024
+                            </div>
+                        </td>
+                        <td>
+                            <div className="py-2 pl-5 pr-10">
+                                <h1 className="font-bold text-2xl">Bachelor Degree of Electrical Engineering</h1>
+                                <p className="text-gray-500">GPA : 3.25</p>
+                                <p className="text-gray-500">focusing in computer science and network engineering</p>
+                            </div>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="">
+                            <div className="w-2 h-2 bg-white border rounded-full"></div>
+                        </td>
+                        <td className="flex justify-center">
+                            <div className="w-full h-2 bg-white border rounded-full"></div>
+                        </td>
+                        <td className="">
+                            <div className="w-2 h-2 bg-white border rounded-full"></div>
+                        </td>
+                        <td className="flex justify-center">
+                            <div className="w-full h-2 bg-white border rounded-full"></div>
+                        </td>
+                        <td className="">
+                            <div className="w-2 h-2 bg-white border rounded-full"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td className="pt-2 align-top">
+                            <div className="rotate-90 h-2 w-2 translate-x-[300%]">
+                                2024-present
+                            </div>
+                        </td>
+                        <td>
+                            <div className="py-2 pl-5 pr-10">
+                                <h1 className="font-bold text-2xl">Null</h1>
+                                <p className="text-gray-500">Unemployed</p>
+                            </div>
+                        </td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 
