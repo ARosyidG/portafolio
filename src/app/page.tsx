8b28727c668ea6a7cac0a24698619e9ec3fc68b9
arosyidg <rosyidkeren164@gmail.com>
2024-12-11 18:04:12 +0800

    fix nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
-import Navbar from "./Navbar";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 export default function Home() {
-  const route = useRouter();
-  useEffect(()=>{route.push("/About")},[]);
+  redirect("/About");
 }
--- src/middleware.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/About', request.url))
-}
- 
-export const config = {
-  matcher: '/',
-}
