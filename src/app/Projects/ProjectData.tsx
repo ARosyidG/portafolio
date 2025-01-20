cd696bdd0c478f58405b16bee0570cad293d509a
Ahmad Rosyid Ganausi <Rosyidkeren164@gmail.com>
2025-01-20 14:29:16 +0800

    Update ProjectData.tsx

--- src/app/Projects/ProjectData.tsx
+++ src/app/Projects/ProjectData.tsx
@@ -112,21 +112,20 @@ export interface IProjectList {
       <div>
         <p>The script is written in Godot and designed to procedurally generate terrain by assigning a specific 3D model (created in Blender) to each tile in a grid, following a set of predefined rules. The rules are stored in a dictionary, where each entry consists of:</p>
         <ul>
-          <li>A unique identifier (ID) for a model/mesh.</li>
-          <li>A pattern representing possible neighboring tile configurations.</li>
+          <li>1. A unique identifier (ID) for a model/mesh.</li>
+          <li>2. A pattern representing possible neighboring tile configurations.</li>
         </ul>
         <p>The neighbor configuration for each tile is represented as a 3x3 array (including the current tile) using binary values:</p>
         <ul>
-          <li>1 indicates the presence of a neighbor.</li>
-          <li>0 indicates the absence of a neighbor.</li>
+          <li>1. 1 indicates the presence of a neighbor.</li>
+          <li>2. 0 indicates the absence of a neighbor.</li>
         </ul>
         <p>For every tile in the grid:</p>
-        <u>
-          <li>The script scans the surrounding tiles to generate an array that describes the neighbor configuration.</li>
-          <li>This array is then matched against the patterns stored in the dictionary to find a corresponding model/mesh ID and, optionally, its rotation.</li>
-          <li>The selected model/mesh is assigned to the tile in the grid.</li>
-        </u>
-        <p>This method is inspired by the Wave Function Collapse algorithm, where each tile in the grid is assigned a model based on its neighboring tiles configurations. The algorithm scans the neighbors (horizontal, vertical, and diagonal) and generates an array representing the neighbors relationships. It then looks up this array in a dictionary to find the corresponding model and any necessary rotations, assigning the model to the tile in the grid. This process ensures that each tile follows specific adjacency rules, similar to how the WFC algorithm enforces constraints in its tile selection.</p>
+        <ul>
+          <li>1.The script scans the surrounding tiles to generate an array that describes the neighbor configuration.</li>
+          <li>2.This array is then matched against the patterns stored in the dictionary to find a corresponding model/mesh ID and, optionally, its rotation.</li>
+          <li>3.The selected model/mesh is assigned to the tile in the grid.</li>
+        </ul>
         <Image className="px-4 py-1" src="https://ganausi.com/image/wfcscreenshoot.png" alt="WFC" width={1000} height={100}></Image>
       </div>
     ),
